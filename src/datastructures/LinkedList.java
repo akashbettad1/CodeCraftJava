de9094d4c983aa7467b3a26d9ef11bbdf9a322b3
akashbettad1 <akashbettad@gmail.com>
2024-12-04 11:42:30 +0100

    LinkedList implementation

---
+++ src/datastructures/LinkedList.java
@@ -0,0 +1,50 @@
+package datastructures;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedList {
+
+    Node head;
+
+    // Add node to the linkedList
+    public void append (int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public void display() {
+        Node current = head;
+        while (current.next != null) {
+            System.out.println(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+
+        System.out.println("Original LinkedList:");
+        list.display();
+    }
+}
